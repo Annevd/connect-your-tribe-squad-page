697c5f62947ed6e9c009e8c487e98f340d5acaf2
Annevd <144004647+Annevd@users.noreply.github.com>
2024-02-27 13:53:26 +0100

    V5
    combined Daan's game + adapted styling

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-// 1. Opzetten van de webserver
+/*** Express setup & start ***/
 
-// Importeer het npm pakket express uit de node_modules map
-import express from 'express'
+// 1. Opzetten van de webserver
 
 // Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
 import fetchJson from './helpers/fetch-json.js'
 
+// Importeer het npm pakket express uit de node_modules map
+import express from 'express'
+
 // Stel het basis endpoint in
 const apiUrl = 'https://fdnd.directus.app/items'
-
-// Haal alle squads uit de WHOIS API op
-const squadData = await fetchJson(apiUrl + '/squad')
 
 // Maak een nieuwe express app aan
 const app = express()
 
 // Stel ejs in als template engine
+// View engine zorgt ervoor dat data die je ophaalt uit de api , waar je in je code dingen mee doet, daar html van maakt
 app.set('view engine', 'ejs')
 
 // Stel de map met ejs templates in
@@ -23,10 +23,19 @@ app.set('views', './views')
 
 // Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
 app.use(express.static('public'))
+
+// Zorg dat werken met request data makkelijker wordt
 app.use(express.urlencoded({ extended: true }));
+
+
+/*** Routes & data ***/
 
 // 2. Routes die HTTP Request and Responses afhandelen
 
+// Haal alle squads uit de WHOIS API op
+const squadData = await fetchJson(apiUrl + '/squad')
+
+// Zet een array klaar, waarin we alle globale berichten voor ons message board op gaan slaan
 const messages = []
 
 // Maak een GET route voor de index
@@ -42,23 +51,81 @@ app.get('/', function (request, response) {
 
     // Stap 4
     // HTML maken op basis van JSON data
-    response.render('index', {persons: persons.data, squads: squadData.data})
+    response.render('index', {
+      persons: persons.data,
+      squads: squadData.data})
   })
-})
-
-// Maak een POST route voor de index
-app.post('/person/:id', function (request, response) {
-  // Er is nog geen afhandeling van POST, redirect naar GET op /
-  messages.push(request.body.bericht)
-  response.redirect(303, '/person/'+ request.params.id)
 })
 
 // Maak een GET route voor een detailpagina met een request parameter id
 app.get('/person/:id', function (request, response) {
   // Gebruik de request parameter id en haal de juiste persoon uit de WHOIS API op
   fetchJson(apiUrl + '/person/' + request.params.id).then((apiData) => {
+
+    // Het custom field is een String, dus die moeten we eerst omzetten (= parsen)
+    // naar een Object, zodat we er mee kunnen werken
+    try {
+      apiData.data.custom = JSON.parse(apiData.data.custom)
+    } catch (e) {}
+
     // Render person.ejs uit de views map en geef de opgehaalde data mee als variable, genaamd person
-    response.render('person', {person: apiData.data, squads: squadData.data, messages: messages})
+    response.render('person', {
+      person: apiData.data,
+      squads: squadData.data,
+      messages: messages})
+  })
+})
+
+// Maak een POST route voor de index
+// Als we vanuit de browser een POST doen op de detailpagina van een persoon
+app.post('/person/:id', function(request, response) {
+
+  // Stap 1: Haal de huidige data op, zodat we altijd up-to-date zijn, en niks weggooien van anderen
+
+  // Haal eerst de huidige gegevens voor deze persoon op, uit de WHOIS API
+  fetchJson(apiUrl + request.params.id).then((apiResponse) => {
+
+    // Het custom field is een String, dus die moeten we eerst
+    // omzetten (= parsen) naar een Object, zodat we er mee kunnen werken
+    try {
+      apiResponse.data.custom = JSON.parse(apiResponse.data.custom)
+    } catch (error) {
+      apiResponse.data.custom = {}
+    }
+
+    // Stap 2: Gebruik de data uit het formulier
+    // Deze stap zal voor iedereen net even anders zijn, afhankelijk van de functionaliteit
+
+    // Controleer eerst welke actie is uitgevoerd, aan de hand van de submit button
+    // Dit kan ook op andere manieren, of in een andere POST route
+    if (request.body.actie == 'verstuur') {
+
+      // Als het custom object nog geen messages Array als eigenschap heeft, voeg deze dan toe
+      if (!apiResponse.data.custom.messages) {
+        apiResponse.data.custom.messages = []
+      }
+
+      // Voeg een nieuwe message toe voor deze persoon, aan de hand van het bericht uit het formulier
+      apiResponse.data.custom.messages.push(request.body.message)
+    }
+
+
+    // Stap 3: Sla de data op in de API
+
+    // Voeg de nieuwe lijst messages toe in de WHOIS API,
+    // via een PATCH request
+    fetch(apiUrl + '/person/' + request.params.id, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        custom: apiResponse.data.custom
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    }).then((patchResponse) => {
+      // Redirect naar de persoon pagina
+      response.redirect(303, '/person/:id' + request.params.id)
+    })
   })
 })
 
@@ -78,6 +145,18 @@ app.get('/filter/:q', function (request, response) {
       response.render('filter', {persons: apiData.data, squads: squadData.data})
   })
 })
+
+// Custom routes: Game
+
+app.get('/app', function(req, res) {
+  fetchJson(apiUrl + '/person?filter={"avatar":{"_nempty":""}}').then((apiData) => {
+    // apiData bevat gegevens van alle personen uit alle squads
+    // Je zou dat hier kunnen filteren, sorteren, of zelfs aanpassen, voordat je het doorgeeft aan de view
+
+    // Render index.ejs uit de views map en geef de opgehaalde data mee als variabele, genaamd persons
+    res.render('app', {persons: apiData.data, squads: squadData.data})
+  })
+});
 
 // 3. Start de webserver
 
