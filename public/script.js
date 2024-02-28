ed49e1a96a975bacf536995b33daa11764b7a245
Daan645 <daan@videotechniek.nl>
2024-02-28 13:33:40 +0100

    pop-up for messages

--- public/script.js
+++ public/script.js
@@ -12,7 +12,7 @@ let messagesCheck = false;
 // wanneer er op de knop berichten wordt geklikt
 document.getElementById('personsCardMessagesActivate').addEventListener('click', function() {
     if (personsMessageCheck === false) {
-messagesActive();
+        messagesActive();
     } else {
         messageNonActive();
     }
@@ -23,18 +23,52 @@ document.getElementById('personsCardMessagesDeactivate').addEventListener('click
     messageNonActive();
 });
 
+document.getElementById('personsCardMessagesDeactivate').addEventListener('click', function() {
+    messageNonActive();
+});
+
+
+
+
+// wanneer er op de knop berichten wordt geklikt
+document.getElementById('personsCardSeeMessages').addEventListener('click', function() {
+    if (personsMessageCheck === false) {
+        messagesCheckActive();
+    } else {
+        messageCheckNonActive();
+    }
+});
+
+// wanneer erop sluit wordt geklikt in berichten
+document.getElementById('personsCardMessagesClose').addEventListener('click', function() {
+    messageCheckNonActive();
+});
+
+
+
+// laat person message verschijnen op het beeld
+function messagesActive() {
+    personsMessageCheck = true;
+    personsMessages.classList.add('persons-card-messages-active');
+}
+
+// laat person message verwijderen van het beeld
+function messageNonActive() {
+    personsMessageCheck = false;
+    personsMessages.classList.remove('persons-card-messages-active');
+}
 
 
 
 // laat person message verschijnen op het beeld
 function messagesCheckActive() {
     messagesCheck = true;
-    personsMessageCheck.classList.add('persons-card-messages-active');
+    personMessagesCheck.classList.add('persons-card-check-messages-active');
 }
 
 // laat person message verwijderen van het beeld
 function messageCheckNonActive() {
     messagesCheck = false;
-    personsMessageCheck.classList.remove('persons-card-messages-active');
+    personMessagesCheck.classList.remove('persons-card-check-messages-active');
 }
  
